1b4712afa0feb03746d4c57ec31326f0835e2cf9
Ramagammer <84947133+Ramagammer@users.noreply.github.com>
2023-10-06 13:29:59 -0300

    Add files via upload

---
+++ passwordCreate.py
@@ -0,0 +1,18 @@
+import random
+from werkzeug.security import generate_password_hash
+
+minus = "abcdefghijklmnopqrstuvwxyz"
+mayus = minus.upper()
+numeros = "0123456789"
+simbolos = "|°!#$%&/()='?¡¿¨´*+[{}]-_.:,;"
+
+base = minus + mayus + numeros + simbolos
+longitud = 12
+
+for i in range(100):
+    muestra = random.sample(base,longitud)
+    password = "".join(muestra)
+    password_encriptado = generate_password_hash(password)
+    print("{} => {}".format(password,password_encriptado))
+
+
